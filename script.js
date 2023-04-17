87967cdb9ba8b601ef3a806bd42374180cdb7802
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-17 10:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal){
+    let binary="";
+    while (decimal>0){
+        let reminder=decimal%2;
+        binary+=reminder;
+        decimal=Math.floor(decimal/2);
+    }
+    return binary;
 }
-
-module.exports = threeSum;
