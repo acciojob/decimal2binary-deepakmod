26fdcd4248519bf076cf964fb56ceec121186a1e
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function decimalToBinary(decimal) {
-  let binary = "";
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary || "0";
+function decimalToBinary(num) {
+  // Write your code here
 }
+
+window.decimalToBinary = decimalToBinary;
